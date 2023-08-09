d39ff64d941a70eca6bee7596323adf1f9eae079
Mohammad Farhad <134843834+farhadctg222@users.noreply.github.com>
2023-08-09 23:28:46 +0600

    ddd
    ddd

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Header from './componet/Header/Header';
+import Produc from './componet/Product/Produc';
+import Shop from './componet/Shop/Shop';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header></Header>
+      
+      <Shop></Shop>
+      
+      
+      <Produc></Produc>
+    </div>
+    
+  );
+}
+
+export default App;
